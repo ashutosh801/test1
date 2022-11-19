bec4fcfceaf08cf3f47ec5282e384849d30d8f58
Ashutosh <ashutosh.nexgen@gmail.com>
2022-11-19 16:03:09 +0530

    updation in sum, diff, multiply

--- diff.java
+++ diff.java
@@ -1,3 +1,8 @@
 public class diff {
-
+    public static void main(String args[]) {
+        int a, b;
+        a = 56;
+        b = 45;
+        System.out.println("difference of two value :" + (a - b));
+    }
 }
--- multiply.java
+++ multiply.java
@@ -1,3 +1,8 @@
 public class multiply {
-
+    public static void main(String args[]) {
+        int a, b;
+        a = 56;
+        b = 45;
+        System.out.println("product of two value :" + (a * b));
+    }
 }
